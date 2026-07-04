8ec16965767914428e6f365e34672f8437ea4770
Eduardo <eduardorn1111@gmail.com>
2026-07-04 04:25:18 -0600

    Ejercicio 10

---
+++ ejercicio10/.antlr/Expr.java
@@ -0,0 +1,141 @@
+// Generated from c:/Users/lalor/OneDrive/Documentos/GitHub/7B_Lenguajes_y_automatas_2/ejercicio10/Expr.g4 by ANTLR 4.13.1
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.misc.*;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast", "CheckReturnValue", "this-escape"})
+public class Expr extends Lexer {
+	static { RuntimeMetaData.checkVersion("4.13.1", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		PRINT=1, CADENA=2, PAR_DE=3, PAR_IZ=4, PUNTCM=5, WS=6;
+	public static String[] channelNames = {
+		"DEFAULT_TOKEN_CHANNEL", "HIDDEN"
+	};
+
+	public static String[] modeNames = {
+		"DEFAULT_MODE"
+	};
+
+	private static String[] makeRuleNames() {
+		return new String[] {
+			"PRINT", "CADENA", "PAR_DE", "PAR_IZ", "PUNTCM", "WS"
+		};
+	}
+	public static final String[] ruleNames = makeRuleNames();
+
+	private static String[] makeLiteralNames() {
+		return new String[] {
+			null, "'print'", null, "'('", "')'", "';'"
+		};
+	}
+	private static final String[] _LITERAL_NAMES = makeLiteralNames();
+	private static String[] makeSymbolicNames() {
+		return new String[] {
+			null, "PRINT", "CADENA", "PAR_DE", "PAR_IZ", "PUNTCM", "WS"
+		};
+	}
+	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+
+	public Expr(CharStream input) {
+		super(input);
+		_interp = new LexerATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+
+	@Override
+	public String getGrammarFileName() { return "Expr.g4"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public String[] getChannelNames() { return channelNames; }
+
+	@Override
+	public String[] getModeNames() { return modeNames; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public static final String _serializedATN =
+		"\u0004\u0000\u0006)\u0006\uffff\uffff\u0002\u0000\u0007\u0000\u0002\u0001"+
+		"\u0007\u0001\u0002\u0002\u0007\u0002\u0002\u0003\u0007\u0003\u0002\u0004"+
+		"\u0007\u0004\u0002\u0005\u0007\u0005\u0001\u0000\u0001\u0000\u0001\u0000"+
+		"\u0001\u0000\u0001\u0000\u0001\u0000\u0001\u0001\u0001\u0001\u0005\u0001"+
+		"\u0016\b\u0001\n\u0001\f\u0001\u0019\t\u0001\u0001\u0001\u0001\u0001\u0001"+
+		"\u0002\u0001\u0002\u0001\u0003\u0001\u0003\u0001\u0004\u0001\u0004\u0001"+
+		"\u0005\u0004\u0005$\b\u0005\u000b\u0005\f\u0005%\u0001\u0005\u0001\u0005"+
+		"\u0001\u0017\u0000\u0006\u0001\u0001\u0003\u0002\u0005\u0003\u0007\u0004"+
+		"\t\u0005\u000b\u0006\u0001\u0000\u0001\u0003\u0000\t\n\r\r  *\u0000\u0001"+
+		"\u0001\u0000\u0000\u0000\u0000\u0003\u0001\u0000\u0000\u0000\u0000\u0005"+
+		"\u0001\u0000\u0000\u0000\u0000\u0007\u0001\u0000\u0000\u0000\u0000\t\u0001"+
+		"\u0000\u0000\u0000\u0000\u000b\u0001\u0000\u0000\u0000\u0001\r\u0001\u0000"+
+		"\u0000\u0000\u0003\u0013\u0001\u0000\u0000\u0000\u0005\u001c\u0001\u0000"+
+		"\u0000\u0000\u0007\u001e\u0001\u0000\u0000\u0000\t \u0001\u0000\u0000"+
+		"\u0000\u000b#\u0001\u0000\u0000\u0000\r\u000e\u0005p\u0000\u0000\u000e"+
+		"\u000f\u0005r\u0000\u0000\u000f\u0010\u0005i\u0000\u0000\u0010\u0011\u0005"+
+		"n\u0000\u0000\u0011\u0012\u0005t\u0000\u0000\u0012\u0002\u0001\u0000\u0000"+
+		"\u0000\u0013\u0017\u0005\"\u0000\u0000\u0014\u0016\t\u0000\u0000\u0000"+
+		"\u0015\u0014\u0001\u0000\u0000\u0000\u0016\u0019\u0001\u0000\u0000\u0000"+
+		"\u0017\u0018\u0001\u0000\u0000\u0000\u0017\u0015\u0001\u0000\u0000\u0000"+
+		"\u0018\u001a\u0001\u0000\u0000\u0000\u0019\u0017\u0001\u0000\u0000\u0000"+
+		"\u001a\u001b\u0005\"\u0000\u0000\u001b\u0004\u0001\u0000\u0000\u0000\u001c"+
+		"\u001d\u0005(\u0000\u0000\u001d\u0006\u0001\u0000\u0000\u0000\u001e\u001f"+
+		"\u0005)\u0000\u0000\u001f\b\u0001\u0000\u0000\u0000 !\u0005;\u0000\u0000"+
+		"!\n\u0001\u0000\u0000\u0000\"$\u0007\u0000\u0000\u0000#\"\u0001\u0000"+
+		"\u0000\u0000$%\u0001\u0000\u0000\u0000%#\u0001\u0000\u0000\u0000%&\u0001"+
+		"\u0000\u0000\u0000&\'\u0001\u0000\u0000\u0000\'(\u0006\u0005\u0000\u0000"+
+		"(\f\u0001\u0000\u0000\u0000\u0003\u0000\u0017%\u0001\u0006\u0000\u0000";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
---
+++ ejercicio10/Expr.py
@@ -0,0 +1,63 @@
+# Generated from ./Expr.g4 by ANTLR 4.13.2
+from antlr4 import *
+from io import StringIO
+import sys
+if sys.version_info[1] > 5:
+    from typing import TextIO
+else:
+    from typing.io import TextIO
+
+
+def serializedATN():
+    return [
+        4,0,6,41,6,-1,2,0,7,0,2,1,7,1,2,2,7,2,2,3,7,3,2,4,7,4,2,5,7,5,1,
+        0,1,0,1,0,1,0,1,0,1,0,1,1,1,1,5,1,22,8,1,10,1,12,1,25,9,1,1,1,1,
+        1,1,2,1,2,1,3,1,3,1,4,1,4,1,5,4,5,36,8,5,11,5,12,5,37,1,5,1,5,1,
+        23,0,6,1,1,3,2,5,3,7,4,9,5,11,6,1,0,1,3,0,9,10,13,13,32,32,42,0,
+        1,1,0,0,0,0,3,1,0,0,0,0,5,1,0,0,0,0,7,1,0,0,0,0,9,1,0,0,0,0,11,1,
+        0,0,0,1,13,1,0,0,0,3,19,1,0,0,0,5,28,1,0,0,0,7,30,1,0,0,0,9,32,1,
+        0,0,0,11,35,1,0,0,0,13,14,5,112,0,0,14,15,5,114,0,0,15,16,5,105,
+        0,0,16,17,5,110,0,0,17,18,5,116,0,0,18,2,1,0,0,0,19,23,5,34,0,0,
+        20,22,9,0,0,0,21,20,1,0,0,0,22,25,1,0,0,0,23,24,1,0,0,0,23,21,1,
+        0,0,0,24,26,1,0,0,0,25,23,1,0,0,0,26,27,5,34,0,0,27,4,1,0,0,0,28,
+        29,5,40,0,0,29,6,1,0,0,0,30,31,5,41,0,0,31,8,1,0,0,0,32,33,5,59,
+        0,0,33,10,1,0,0,0,34,36,7,0,0,0,35,34,1,0,0,0,36,37,1,0,0,0,37,35,
+        1,0,0,0,37,38,1,0,0,0,38,39,1,0,0,0,39,40,6,5,0,0,40,12,1,0,0,0,
+        3,0,23,37,1,6,0,0
+    ]
+
+class Expr(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    PRINT = 1
+    CADENA = 2
+    PAR_DE = 3
+    PAR_IZ = 4
+    PUNTCM = 5
+    WS = 6
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "'print'", "'('", "')'", "';'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "PRINT", "CADENA", "PAR_DE", "PAR_IZ", "PUNTCM", "WS" ]
+
+    ruleNames = [ "PRINT", "CADENA", "PAR_DE", "PAR_IZ", "PUNTCM", "WS" ]
+
+    grammarFileName = "Expr.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.13.2")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
