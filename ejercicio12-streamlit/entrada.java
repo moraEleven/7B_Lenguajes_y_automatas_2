2889dcd514b5c0d6589877d9ed7c11ab60726e16
Eduardo <eduardorn1111@gmail.com>
2026-07-04 22:23:14 -0600

    Ejercicio 12 Streamlit

---
+++ ejercicio12-streamlit/entrada.java
@@ -0,0 +1,10 @@
+public class entrada {
+    public static void main(String[] args) {
+        int edad = 18;
+        String adulto = "No";
+        if (edad >= 18) {
+            adulto = "Si";
+        }
+        System.out.println("¿Es adulto?: " + adulto);
+    }
+}
