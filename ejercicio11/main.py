b3ebf1199c3675e5697407ffdbf1899e6b18f1f1
Eduardo <eduardorn1111@gmail.com>
2026-07-04 21:11:43 -0600

    Ejercicio 11

---
+++ ejercicio11/main.py
@@ -0,0 +1,29 @@
+# Numero de control: TU_NUMERO_DE_CONTROL
+from archivo import Archivo
+from analizador_lexico import AnalizadorLexico
+
+class Main:
+    def __init__(self):
+        self.analizador = AnalizadorLexico()
+
+    def ejecutar(self):
+        ruta = input("Escribe la ruta del archivo: ")
+        archivo = Archivo(ruta, extension_esperada=".java")
+        if not archivo.existe():
+            print("El archivo no existe")
+            return
+        if not archivo.es_el_tipo_correcto():
+            print("El archivo debe ser .java")
+            return
+        codigo = archivo.leer()
+        archivo.imprimir_info()
+        print("\nCODIGO ORIGINAL")
+        print("-" * 40)
+        print(codigo)
+        self.analizador.analizar(codigo)
+        self.analizador.imprimir_tokens()
+        self.analizador.imprimir_errores()
+
+if __name__ == "__main__":
+    app = Main()
+    app.ejecutar()
